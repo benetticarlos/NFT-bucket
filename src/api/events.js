ee6964d3a2131483964895b49c914f3f6b00efc1
Carlos Benetti <benetticarlos1@gmail.com>
2022-03-30 14:09:48 -0300

    set config

--- src/api/events.js
+++ src/api/events.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import config from '../config/config';
 
 export default async function getEvents(asset_contract_address, token_id) {
   const options = {
@@ -10,7 +11,7 @@ export default async function getEvents(asset_contract_address, token_id) {
     },
     headers: {
       Accept: 'application/json',
-      'X-API-KEY': '25d2252ed8444f49aeafd5ad5339cc34',
+      'X-API-KEY': config.OPENSEA_API_KEY,
     },
   };
 
---
+++ src/config/config.js
@@ -0,0 +1,7 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const config = {
+  OPENSEA_API_KEY: process.env.OPENSEA_API_KEY,
+};
+export default config;
